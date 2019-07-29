0a3eb9111b025c6a00c70e64f7f36dfceb015ed6
ljy644419778 <644419778@qq.com>
2019-07-29 22:23:36 +0800

    fragment切换 返回栈 传值

--- app/src/main/java/com/example/myapplication/fragment/AFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/AFragment.java
@@ -1,11 +1,12 @@
 package com.example.myapplication.fragment;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +17,9 @@ import com.example.myapplication.R;
 
 public class AFragment extends Fragment {
     private TextView mTvTitle;
+    private Button mBtnChange,mBtnReSet,mBtnMessage;
+    private BFragment bFragment;
+    private IOnMessageClick iOnMessageClick;
     //fragment传参
     public static AFragment newInstance(String title){
         AFragment fragment = new AFragment();
@@ -24,11 +28,28 @@ public class AFragment extends Fragment {
         fragment.setArguments(bundle);//传参用这个
         return fragment;
     }
+
+    public interface IOnMessageClick{
+        void onClick(String text);
+    }
+
+    //当Fragment被依附到Activity时候会调用这个方法，所以写在这里
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            iOnMessageClick = (IOnMessageClick) context;
+        }catch (ClassCastException e){
+            throw  new ClassCastException("Activity 必须实现iOnMessageClick接口");
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        //设置布局文件
         View view=inflater.inflate(R.layout.fragement_a,container,false);
+        Log.d("AFragment","-----------onCreateView--------------");
         return view;
     }
 
@@ -37,6 +58,41 @@ public class AFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //加载布局后进行的操作
         mTvTitle=view.findViewById(R.id.tv_fga_title);
+        mBtnChange=view.findViewById(R.id.btn_changeFg);
+        mBtnReSet=view.findViewById(R.id.btn_reSet);
+        mBtnMessage=view.findViewById(R.id.btn_message);
+        mBtnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bFragment==null){
+                    bFragment=new BFragment();
+                }
+
+                Fragment fragment=getFragmentManager().findFragmentByTag("a");
+                if (fragment!=null)
+                {
+                    //先隐藏前一个fragment在加载新的fragment（在前一个fragment更改的视图也会被保留），addToBackStack(null) 添加到返回栈。点返回键就会返回上个fragment
+                    getFragmentManager().beginTransaction().hide(fragment).add(R.id.fl_container,bFragment).addToBackStack(null).commitAllowingStateLoss();
+                }else {
+                    getFragmentManager().beginTransaction().replace(R.id.fl_container, bFragment).addToBackStack(null).commitAllowingStateLoss();
+                }
+            }
+        });
+
+        mBtnReSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTvTitle.setText("我是新的内容");
+            }
+        });
+
+        mBtnMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //((ContainerActivity)getActivity()).SetData("你好"); 不推荐这样传值
+                iOnMessageClick.onClick("你好");//实现回调接口来传值
+            }
+        });
         if (getArguments()!=null){
             mTvTitle.setText(getArguments().getString("title"));
         }
--- app/src/main/java/com/example/myapplication/fragment/ContainerActivity.java
+++ app/src/main/java/com/example/myapplication/fragment/ContainerActivity.java
@@ -5,33 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
-public class ContainerActivity extends AppCompatActivity {
+public class ContainerActivity extends AppCompatActivity implements AFragment.IOnMessageClick {
 
     private AFragment aFragment;
-    private BFragment bFragment;
-    private Button mBtnChange;
+    private TextView mTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-        mBtnChange=findViewById(R.id.btn_changeFg);
-        mBtnChange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (bFragment==null) {
-                bFragment = new BFragment();
-                }
-                //这里更换用replace
-                getSupportFragmentManager().beginTransaction().replace(R.id.fl_container,bFragment).commitAllowingStateLoss();
-            }
-        });
+        mTitle=findViewById(R.id.tv_container_title);
         //实例化AFragment
         aFragment =  AFragment.newInstance("我是参数");
         //把AFragment添加到Activity中  commitAllowingStateLoss比 commit宽容些不容易出错
-        getSupportFragmentManager().beginTransaction().add(R.id.fl_container,aFragment).commitAllowingStateLoss();
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_container,aFragment,"a").commitAllowingStateLoss();
     }
 
+    public void SetData(String text){
+        mTitle.setText(text);//不推荐这种传值方法
+    }
+
+    //推荐用实现Afragment里面的回调接口来传值
+    @Override
+    public void onClick(String text) {
+        mTitle.setText(text);
+    }
 }
