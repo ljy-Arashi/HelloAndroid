ce107baff5cab4bcc2a4b895ab0374a2f20522ef
ljy644419778 <644419778@qq.com>
2019-08-16 23:01:50 +0800

    NavigationView

--- app/src/main/java/com/example/myapplication/materialDesign/MaterialDesignActivity.java
+++ app/src/main/java/com/example/myapplication/materialDesign/MaterialDesignActivity.java
@@ -1,5 +1,10 @@
 package com.example.myapplication.materialDesign;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,22 +13,21 @@ import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
 import com.example.myapplication.R;
+import com.example.myapplication.util.ToastUtil;
+import com.google.android.material.navigation.NavigationView;
 
 public class MaterialDesignActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_design);
         Toolbar toolbar=findViewById(R.id.ui_toolbar);
         mDrawerLayout =findViewById(R.id.drawer_layout);
+        mNavigationView=findViewById(R.id.nav_view);
         toolbar.setTitle("FaceBook");//设置标题
         toolbar.setLogo(R.drawable.facebook_logo);//设置logo
         //toolbar.setSubtitle("by Arashi");//设置副标题
@@ -38,6 +42,31 @@ public class MaterialDesignActivity extends AppCompatActivity {
             //设置返回home图标
             actionBar.setHomeAsUpIndicator(R.drawable.toolbar_menu2);
         }
+        mNavigationView.setCheckedItem(R.id.nav_call);//默认选中
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_call:
+                        ToastUtil.showMsg(MaterialDesignActivity.this,"call");
+                        break;
+                    case R.id.nav_friends:
+                        ToastUtil.showMsg(MaterialDesignActivity.this,"friends");
+                        break;
+                    case R.id.nav_location:
+                        ToastUtil.showMsg(MaterialDesignActivity.this,"location");
+                        break;
+                    case R.id.nav_mail:
+                        ToastUtil.showMsg(MaterialDesignActivity.this,"mail");
+                        break;
+                    case R.id.nav_task:
+                        ToastUtil.showMsg(MaterialDesignActivity.this,"task");
+                        break;
+                }
+                mDrawerLayout.closeDrawers();//将滑动菜单关闭
+                return true;
+            }
+        });
     }
 
     //添加menu
